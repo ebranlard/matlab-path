b9fab258a22aa7c6c7008c80af254ada7d3e57d3
elmanuelito <trashy.trashos@gmail.com>
2015-08-30 15:04:44 +0200

    _dispatchFigs more general

--- libs/lib_plot/dispatchFigs.m
+++ libs/lib_plot/dispatchFigs.m
@@ -1,33 +1,92 @@
 function []=dispatchFigs(varargin)
-% Dispatch figures over the screen and not on top of each other
+% Dispatch figures over the screens and not on top of each other
 % E. Branlard June 2013
 %
-% dispatchFigs : no arguments: try his best to put the figure on the screen without resizing figures
+% dispatchFigs    : no arguments: dispatch the figure on the screens without resizing figures
 % dispatchFigs(0) : same as dispatchFigs without arguments
 % dispatchFigs(1) : use a defaults grid on the available screen(s)
 % dispatchFigs(2) : use a default grid on the laptop display (screen 2)
-% dispatchFigs(3,4) : use grid 3x4 on screen 1 
-% dispatchFigs(3,4,2) : use grid 3x4 on screen 2 
+% dispatchFigs(3,4)  : use grid 3x4 on screen 1 
+% dispatchFigs(3,4,2): use grid 3x4 on screen 2 
 % dispatchFigs(3,4,2,2) : use grid 3x4 on screen 1 and 2x2 on screen two
 %
-% Parameters
-hWin=60; %height of matlab toolbar + window manager
-Laptop=[1600 900];
-xMenuBar=110; 
-yMenuBar=0;
+%% Parameters
+%
+hWin=60;      %<<<<<<<< Height of matlab toolbar + window manager
 
-A=get( 0, 'ScreenSize' );
-if(A(3)>Laptop(1))
-    S{1}=[xMenuBar A(4)-yMenuBar A(3)-Laptop(1) A(4)-yMenuBar];
-    S{2}=[A(3)-Laptop(1) A(4) A(3) Laptop(2)];
+% Extent of menu bars:   TODO: Make this screen-wise
+xMenuBarLeft   = 110;
+xMenuBarRight  = 0;
+yMenuBarTop    = 0  ;
+yMenuBarBottom = 0  ;
+
+
+
+% --------------------------------------------------------------------------------
+% ---  Coordinate system adopted in this script
+% --------------------------------------------------------------------------------
+%       ^ y
+%       | 
+%     
+%       |---------------|
+%       |               |
+%       |    SCREEN     |
+%       |               |
+%       |---------------|  -> x
+%  (xmin,ymin)
+%     (0,0)
+%
+%
+%
+%
+%
+
+
+% --------------------------------------------------------------------------------
+% --- Detecting number of screens and screens dimensions 
+% --------------------------------------------------------------------------------
+A  = get(0, 'ScreenSize' )    ; % should disappear
+mp = get(0, 'MonitorPositions');
+nMonitors= size(mp,1);
+% --------------------------------------------------------------------------------
+% --- Creating screens "effective" extents  (without menu bars)
+% --------------------------------------------------------------------------------
+% S{i} = [xmin, yTopCorner, xmax, ymax]
+if(nMonitors==1)
+    % Parameter for the current monitor
+    xmin= mp(1,1);
+    ymin= mp(1,2);
+    lx  = mp(1,3);
+    ly  = mp(1,4);
+    % Reducing these parameters according to menu bars
+    S{1}=[xmin+xMenuBarLeft ymin+ly-yMenuBarTop lx-xMenuBarRight ly-yMenuBarBottom];
+elseif(nMonitors==2) 
+    for i = 1:nMonitors
+        xmin= mp(i,1);
+        ymin= mp(i,2);
+        lx  = mp(i,3);
+        ly  = mp(i,4);
+        % Reducing these parameters according to menu bars
+        S{i}=[xmin+xMenuBarLeft ymin+ly-yMenuBarTop lx-xMenuBarRight ly-yMenuBarBottom];
+    end
+
 else
-    S{1}=[xMenuBar Laptop(2)-yMenuBar Laptop(1) Laptop(2)];
+    error('Script writtent in a naive way, generalization for n monitors todo')
 end
 
-nScreens=length(S);
+%% LEGACY
+% Laptop=[1600 900];
+% if(A(3)>Laptop(1))
+%     S{1}=[xMenuBar A(4)-yMenuBar A(3)-Laptop(1) A(4)-yMenuBar];
+%     S{2}=[A(3)-Laptop(1) A(4) A(3) Laptop(2)];
+% else
+%     S{1}=[xMenuBar Laptop(2)-yMenuBar Laptop(1) Laptop(2)];
+% end
 
-% try to dispatch without resizing figs)
 if(nargin==0 || (nargin==1 && varargin{1}==0))
+    % --------------------------------------------------------------------------------
+    % --- Dispatch without resizing figs)
+    % --------------------------------------------------------------------------------
     iScreen=1;
     ScreenRoom=S{1};
     figs=get(0,'children');
@@ -49,6 +108,10 @@ if(nargin==0 || (nargin==1 && varargin{1}==0))
             ScreenRoom=S{iScreen} ;
          end
      end
+
+     % --------------------------------------------------------------------------------
+     % --- Put figures on a grid 
+     % --------------------------------------------------------------------------------
  elseif(nargin==1)
    % Default Grid
    switch (varargin{1}) 
@@ -86,6 +149,12 @@ else
     disp('wrong number or arguments');
 end
 
+
+
+
+% --------------------------------------------------------------------------------
+% ---  
+% --------------------------------------------------------------------------------
 function []=setFigGrid(S,G,hWin)
 nG=length(G);
 %compute Grid coordinates and store them in GP
@@ -100,9 +169,6 @@ for iG=1:nG
         end
     end
 end
-
-
-
 % 
 iG=1;
 nmax=length(GP{1}(:,1));
